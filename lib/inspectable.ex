10fb56c8b349593a7cc89a62c84c432c5cf06214
Kristian Larsson <kristian@tutamail.com>
2018-10-04 14:42:43 -0700

    Added inspectable defprotocol

---
+++ lib/inspectable.ex
@@ -0,0 +1,18 @@
+defprotocol Inspectable do
+  def dump(element)
+end
+
+defimpl Inspectable, for: BitString do
+  def dump(string) do
+    "STRING: #{string}"
+  end
+end
+
+defimpl Inspectable, for: Integer do
+  def dump(0) do
+    "ZERO!"
+  end
+  def dump(string) do
+    "INTEGER: #{string}"
+  end
+end
